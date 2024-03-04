b30ff4e8b1959864d1c06996aba21bd4851db625
Dimitar Dimitrov <dimitrov.dna@gmail.com>
2024-03-04 16:19:13 +0100

    added plot of historical data

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,41 @@
 
-function plot(data) {
+async function plot(data, historic) {
     console.log(data);
+    console.log(historic);
+
     const time = data['time']
     const nres = data['nres']
     
-    const updatetime = data['updatetime'
-]
+    const updatetime = data['updatetime']
+
+    var firsttime = time[0]
+    var date = new Date()
+    var d = date.getDay()
+
+    console.log(d)
+    var today = historic[d]
+    console.log(today)
+    var starttimeidx = today['times'].indexOf(firsttime)
+    console.log(starttimeidx)
+
+    var historictime = today['times'].slice(starttimeidx)
+    var historicnres = today['mres'].slice(starttimeidx)
+
+
     const trace = {
         x: time,
         y: nres,
         mode: 'lines+marekrs',
-        type: 'scatter'
+        type: 'scatter',
+        name: 'Current<br>Reservations'
+    };
+
+    const historictrace = {
+        x: historictime,
+        y: historicnres,
+        mode: 'lines+marekrs',
+        type: 'scatter',
+        name: 'Historic<br>Reservations'
     };
 
     const layout = {
@@ -31,16 +56,22 @@ function plot(data) {
         
     };
     
-    Plotly.newPlot('plotlydiv', [trace], layout);
+    Plotly.newPlot('plotlydiv', [trace, historictrace], layout);
 }
 
-fetch('today.json')
-    .then(response => response.json())
-    .then(data => {
-        plot(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+async function loadjson(filename){
+    return fetch(filename)
+        .then(response => response.json())
+        .then(responseJson => { return responseJson;})
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
 
+async function caller(){
+    const historic = await loadjson('histdict.json');
+    const today = await loadjson('today.json');
+    plot(today, historic);
+}
 
+caller()
